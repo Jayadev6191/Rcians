737497e2850214c9f5b679d657d4ec7f9a52aec4
Jayadeva <jayadeva@reliablecoders.com>
2014-08-08 11:58:04 -0700

    added css to box container

--- application/views/meals.php
+++ application/views/meals.php
@@ -4,15 +4,26 @@
 		<li>{{meal}}</li>
 	</ul>
 </script>
-<div class="container box1 effect8">
-    <h3>Effect 8</h3>
+<div class="box effect8">
+   <h3>Choose your meals</h3>
+   <div class="form-group">
+	<div class="form-group">
+		<button type="button" class="btn btn-danger" style="float:right" id="submit_meal">Go</button>
+	    <input type="text" class="form-control" name="mealname" id="meal_name" placeholder="Enter your favourite food"></input>
+	</div>
 </div>
+   
+   <div class="selected_choices"></div>
+ 
+</div>
+  
 <div id="meals_container" class="container box effect8">
   <h3>Choose your meals</h3>
 	<div class="form-group">
-		<button type="button" class="btn btn-danger" style="float: right" id="submit_meal">Go</button>
+		<button type="button" class="btn btn-danger" style="float:right" id="submit_meal">Go</button>
 	    <input type="text" class="form-control" name="mealname" id="meal_name" placeholder="Enter your favourite food"></input>
 	</div>
+	
 	
 	<div class="selected_choices"></div>
     <!-- <div style="overflow: hidden; padding-right: .5em;">
@@ -23,3 +34,5 @@
 	</div>
 </div>
 <script type="text/javascript" src="public/js/meals.js"></script>
+
+
