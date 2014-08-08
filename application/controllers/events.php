18bd35c0c0896b31138d05171ba372163f5b5bad
Gazal Chug <gazalc@reliablecoders.com>
2014-08-08 10:46:28 -0700

    Added the Events Page

---
+++ application/controllers/events.php
@@ -0,0 +1,54 @@
+<?php
+if (!defined('BASEPATH'))exit('No direct script access allowed');
+
+class Events extends CI_Controller {
+    // variable
+     public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('managerapi');    
+        $this->load->library('session'); 
+    }
+    public function index() {
+        $this -> overview();
+    }
+
+    public function overview() {
+        $this -> load -> view('template/header');
+        $this -> load -> view('page/events');
+        $this -> load -> view('template/footer');       
+    }
+    
+    
+    /*Upload the file temporarly to */
+  public function upload_news_letter()
+  {
+    //$this->load->helper(array('form','url'));
+    $config['upload_path'] = './uploads/';
+    $config['allowed_types'] = 'pdf|doc';
+    $config['max_size'] = '9000';   /* units in kilobytes */
+    //$config['max_width']  = '2041';
+    //$config['max_height']  = '3072';    
+    $this->load->library('upload', $config);
+    $this->load->helper('url');
+
+    if ( ! $this->upload->do_upload())
+    {
+      $error = array('error' => $this->upload->display_errors());
+      $error['status'] = 0;
+      echo json_encode($error);
+    }
+    else
+    {      
+      $data = array('upload_data' => $this->upload->data());
+      $data['status'] = 1;
+      $data['path'] = base_url() . 'uploads/' . $data['upload_data']['file_name'];      
+      echo json_encode($data);
+    }
+  }
+
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
