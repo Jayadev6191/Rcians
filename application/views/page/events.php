019c2169065d5c3f5629b7203ef036ec62dd696a
Gazal Chug <gazalc@reliablecoders.com>
2014-08-08 11:04:15 -0700

    Added form to create Events

--- application/views/page/events.php
+++ application/views/page/events.php
@@ -31,25 +31,38 @@
         </div>
       </li>                  
     </ul>
-     <button id="upload" class="btn btn-danger" data-toggle="modal" data-target="#uploadModal"  title="Newsletter" alt="Newsletter">Upload News letter</button>       
+     <button id="upload" class="btn btn-danger" data-toggle="modal" data-target="#uploadEvent"  title="Events" alt="Event Upload">Create Event</button>       
 </div>
 <!-- news letter upload modal -->
-    <div class="modal fade" id="uploadModal">
+    <div class="modal fade" id="uploadEvent">
       <div class="modal-dialog">
         <form enctype="multipart/form-data" action="newsletters/upload_news_letter" method="post" >
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-              <h4 class="modal-title">Upload News Letter</h4>
+              <h4 class="modal-title">Create Event</h4>
             </div>
             <div class="modal-body">
-              <p class="upload_error" style="display: none;"></p>
-              <p>Upload News Letter of the Month:</p>
-              <input type="file" required="required" name="userfile" id="file-with-photo">
+              <div class="form-group">
+              <label for="eventName">Event Name</label>              
+              <input type="text" required="required" name="eventname" id="eventname" class="form-control">
+              </div>
+              <div class="form-group">
+              <label for="eventDate">Date</label>              
+              <input type="date" required="required" name="eventDate" id="eventDate" class="form-control">
+              </div>
+              <div class="form-group">
+              <label for="event_start_time">Start Time</label>              
+              <input type="datetime" required="required" name="event_start_time" id="event_start_time" class="form-control">
+              </div>
+              <div class="form-group">
+              <label for="event_end_time">End Time</label>              
+              <input type="text" required="required" name="event_end_time" id="event_end_time" class="form-control">
+              </div>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button id="submitupload" type="button" class="btn btn-primary">Upload</button>
+              <button id="submitupload" type="button" class="btn btn-primary">Create</button>
               <!-- <input type="submit" value="Upload" class="btn btn-primary"> -->
             </div>
           </div><!-- /.modal-content -->
